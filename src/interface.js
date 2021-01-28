2d827d9b31a3cb3530bb358783c29dcb218e9422
Roury Hinds <roury_hinds@outlook.com>
2021-01-28 01:31:40 +0000

    made changes to styling and added dynamic styling to buttons

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+})
+  let $submit = $('input[type="submit"]');
+    
+  $submit.prop('disabled', true);
+      $('input[type="text"]').on('input change', function() { //'input change keyup paste'
+        $submit.prop('disabled', !$(this).val().length);
+  });
+  
   let thermostat = new Thermostat();
   updateTemperature();
 
@@ -29,9 +39,29 @@ $(document).ready(function() {
     updateTemperature();
   })
 
+  displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  let city = $('#current-city').val();
+  displayWeather(city);
+  $('#temp-in-city').text(city);
+})
+
   function updateTemperature() {
-    $('#temperature').text(`temperature ${thermostat.temperature}°`);
+    $('#temperature').text(`temp ${thermostat.temperature}°`);
     $('#temperature').attr('class', thermostat.energyUsage());
     $('#myBar').attr('class', thermostat.energyUsage());
   };
+
+  function displayWeather(city) {
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    let token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    let units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  }
+
+
 });
